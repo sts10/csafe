16517121d120679eba965b813082f101440adaf2
Wesley Moore <wes@wezm.net>
2021-04-25 10:16:18 +1000

    Use HashSet instead of Vec for faster membership tests

--- tests/integration-tests.rs
+++ tests/integration-tests.rs
@@ -1,28 +1,34 @@
 mod integration_tests {
     use ::csafe::*;
+    use std::collections::HashSet;
 
     #[test]
     fn can_find_unsafe_words() {
         let word_list = [
-            "apple".to_string(),
-            "bear".to_string(),
-            "latenight".to_string(),
-            "late".to_string(),
-            "night".to_string(),
-            "good".to_string(),
-            "goodnight".to_string(),
-            "sea".to_string(),
-            "seas".to_string(),
-            "sand".to_string(),
-            "and".to_string(),
-            "zoo".to_string(),
-            "zoology".to_string(),
-        ];
+            "apple",
+            "bear",
+            "latenight",
+            "late",
+            "night",
+            "good",
+            "goodnight",
+            "sea",
+            "seas",
+            "sand",
+            "and",
+            "zoo",
+            "zoology",
+        ]
+        .iter()
+        .map(|&s| s.to_owned())
+        .collect();
+        let mut unsafe_words = find_unsafe_words(&word_list);
+        unsafe_words.sort_by(|a, b| a.first().unwrap().cmp(b.first().unwrap()));
         assert_eq!(
-            find_unsafe_words(&word_list),
+            unsafe_words,
             vec![
-                vec!["late", "night", "latenight"],
                 vec!["good", "night", "goodnight"],
+                vec!["late", "night", "latenight"],
                 vec!["sea", "sand", "seas", "and"],
                 vec!["seas", "and", "sea", "sand"],
             ]
@@ -56,14 +62,23 @@ mod integration_tests {
         ];
         assert_eq!(
             find_fewest_words_to_remove(contenders_for_removal),
-            vec!["night", "sea"]
+            ["night", "sea"]
+                .iter()
+                .map(|&s| s.to_owned())
+                .collect::<HashSet<_>>()
         );
     }
 
     #[test]
     fn can_remove_words_from_a_wordlist() {
-        let list = vec!["bill".to_string(), "harry".to_string(), "ross".to_string()];
-        let words_to_remove = vec!["harry".to_string()];
+        let list = ["bill", "harry", "ross"]
+            .iter()
+            .map(|&s| s.to_owned())
+            .collect::<HashSet<_>>();
+        let words_to_remove = ["harry"]
+            .iter()
+            .map(|&s| s.to_owned())
+            .collect::<HashSet<_>>();
         assert_eq!(
             make_clean_list(words_to_remove, list),
             vec!["bill".to_string(), "ross".to_string()]
