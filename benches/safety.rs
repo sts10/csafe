b67f7a574687470e37a3720b3df23da207701bcc
Sam Schlinkert <sschlinkert@gmail.com>
2021-04-24 21:13:02 -0400

    updates benchmark

--- benches/safety.rs
+++ benches/safety.rs
@@ -3,11 +3,11 @@ use csafe::*;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("FindUnsafe");
-    let unsafe_word_list = make_vec_from_file("./tests/test-files/agile_words_letter_a.txt");
+    let unsafe_word_list = make_set_from_file("./tests/test-files/agile_words_letter_a.txt");
     // let safe_word_list = make_vec_from_file("./tests/test-files/agile_words_letter_a.txt.csafe");
 
     group.sample_size(10);
-    group.bench_function("As implemented", |b| {
+    group.bench_function("Using hashset", |b| {
         b.iter(|| find_unsafe_words(&unsafe_word_list))
     });
 }
